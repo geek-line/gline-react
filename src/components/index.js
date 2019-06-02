24e30e54b5df8a21685ef122e820768d23fa6c8a
今村友哉 <imatomo1234imatomo@gmail.com>
2019-06-02 20:18:14 +0900

    index.js変更(エラー確認)

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../bower_components/materialize/dist/css/materialize.css";
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { db } from '../firebase'
 import Post from "./post";
 import firebase from '../firebase'
 
@@ -11,10 +12,39 @@ class Index extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            
+            course: ''
         }
-        
+        this.handleInput = this.handleInput.bind(this);
+    this.send = this.save.bind(this);
     }
+
+
+    handleInput({ target: { value } }) {
+        this.setState({
+          course: value
+        });
+      }
+      
+    save=(e)=>{
+        e.preventDefault()
+
+        const user = firebase.auth().currentUser
+        if( user ){
+            db.collection("users").doc(user.uid).set({
+                name: user.displayName,
+                pic: user.photoURL || '/images/profile_placeholder.png',
+                email: user.email,
+                course: this.state.course
+            })
+            .then(() => {
+                console.log(`追加に成功しました `);
+            })
+            .catch((error) => {
+                console.log(`追加に失敗しました (${error})`);
+            });
+        }
+    }
+
 
     render(){
         console.log(this.props.user)
@@ -29,11 +59,26 @@ class Index extends React.Component{
                             <li className="header-web"><Link to='/web'>WEB</Link></li>
                             <li className ="header-game"><Link to='/game'>GAME</Link></li>
                             <li className="header-iphone"><Link to='/iphone'>iPhone</Link></li>
+                           
+                           
+                          
                              {/* <Route path='/web' component={Web} />
                         <Route path='/game' component={Game} />
                         <Route path='/iphone' component={Iphone} /> */}
                             
                         </ul>
+                        <div>
+                        <form>
+                        <select name = "course" value={this.state.course} onChange={this.handleInput}>
+                            <option  value="web"> web</option>
+                            <option value="game"> game</option>
+                            <option value="iphone">iphone</option>
+                        </select>
+                          
+                        <button onClick={this.save}>send</button>
+                        
+                        </form>
+                        </div>
                     </div>
                 )
                 :
@@ -60,7 +105,8 @@ class Index extends React.Component{
           <div>
                 <div>名前: {this.props.user && this.props.user.displayName}</div>
                 <div>E-mail: {this.props.user && this.props.user.email}</div>
-                <div>picture: <img src  ={this.props.user.photoURL || '/images/profile_placeholder.png'}></img></div>
+                <div><img src  ={this.props.user.photoURL || '/images/profile_placeholder.png'}></img></div>
+              
                 <li className="gline-logout" onClick={this.logout}><Link to='/login'>ログアウト</Link></li>
                 </div>
                 ) : (
