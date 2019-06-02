a92d5661558b7e7a0645096df454a56140ed0d91
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2019-06-02 14:59:13 +0900

    特になし

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Login from "./components/login";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    
+  }
+   render(){
+    return(
+    <BrowserRouter>
+      <div>
+        <Route exact path='/' component={Login}/>
+      </div>
+  </BrowserRouter>
+    )
+  }
 }
 
 export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../bower_components/materialize/dist/css/materialize.css";
+import "./style.css"
 
 class Login extends React.Component{
     constructor(){
@@ -10,13 +11,11 @@ class Login extends React.Component{
         
     }
 
-
-
     render(){
         
         return(
             <div>
-                 <header>
+                <header class="page-header teal">
                     <div className="header-content">
                         <ul className="header-list">
                             <li className="header-logo">GLINE</li>
@@ -27,13 +26,66 @@ class Login extends React.Component{
                     </div>
                 </header>
                 <div className="main">
+                <div className="row center">
+                    <div className="xl1 l2 m4 s6 col red lighten-5">1</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-4">2</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-3">3</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-2">4</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-1">5</div>
+                    <div className="xl1 l2 m4 s6 col red">6</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-5">7</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-4">8</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-3">9</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-2">10</div>
+                    <div className="xl1 l2 m4 s6 col red lighten-1">11</div>
+                    <div className="xl1 l2 m4 s6 col red">12</div>
+                </div>
+
+                <div class="video-container center">
+                        <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div className="s2 center">
+                    <a class="btn-floating pulse"><i class="material-icons"></i></a>
+                </div>
+                
+
+
+                <div className="row center">
+                    <div className="s2 col">1</div>
+                    <div className="s2 col">2</div>
+                    <div className="s2 col">3</div>
+                    <div className="s2 col">4</div>
+                    <div className="s2 col">5</div>
+                    <div className="s2 col">6</div>
+                </div>
                     
                 </div>
-                <footer>
-                    <div className="footer-content">
-
+        
+                <footer class="page-footer teal">
+                    <div class="container">
+                        <div class="row">
+                        <div class="col l6 s12">
+                            <h5 class="white-text">Footer Content</h5>
+                            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                        </div>
+                        <div class="col l4 offset-l2 s12">
+                            <h5 class="white-text">Links</h5>
+                            <ul>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                            </ul>
+                        </div>
+                        </div>
                     </div>
-                </footer>
+                    <div class="footer-copyright">
+                <div class="container">
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                </div>
+            </div>
+        </footer>
             </div>
         );
     }
